17b22d22937e428c60ead79200c22f36e05910c3
Ameer <46894200+Ameer-N@users.noreply.github.com>
2020-02-21 15:29:35 +0530

    Create Resursion.java

---
+++ Resursion.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Recursion{
+      
+   public static void main(String[] args){
+      Scanner scan=new Scanner(System.in);
+      int no,i=1;
+      System.out.println("Enter the multiplication Number:");
+      no=scan.nextInt();
+      System.out.println(no+" multiplication Table:");
+      if(no>=0)
+          generatemul(no,i);
+      else
+         System.out.println("negative number found");
+    
+    }
+    public static void generatemul(int no,int i){ 
+        if(i<=10){
+            System.out.println(no+" X "+i+" = "+no*i);
+            generatemul(no,++i);
+        }
+        
+    }
+         
+   
+}
