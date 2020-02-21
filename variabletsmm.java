d436353bfbb506e9e3f0ff7f9f57cfdad6882a68
Ameer <46894200+Ameer-N@users.noreply.github.com>
2020-02-21 14:18:05 +0530

    Create variabletsmm.java

---
+++ variabletsmm.java
@@ -0,0 +1,47 @@
+
+public class variabletsmm{
+	public static void main(String[] args){
+    //byte
+        System.out.println(Byte.TYPE);
+        System.out.println(Byte.SIZE/8+"  byte");
+        System.out.println(Byte.MIN_VALUE);
+        System.out.println(Byte.MAX_VALUE);
+    //Character    
+        System.out.println();
+        System.out.println(Character.TYPE);
+        System.out.println(Character.SIZE);
+        System.out.println((int) Character.MIN_VALUE);
+        System.out.println((int) Character.MAX_VALUE);
+    //Short
+         System.out.println();
+        System.out.println(Short.TYPE);
+        System.out.println(Short.SIZE/8+"  byte");
+        System.out.println(Short.MIN_VALUE);
+        System.out.println(Short.MAX_VALUE);
+    //int
+        System.out.println();   
+        System.out.println(Integer.TYPE);
+        System.out.println(Integer.SIZE/8+"  byte");
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+    //long
+        System.out.println(Long.TYPE);
+        System.out.println(Long.SIZE/8+"  byte");
+        System.out.println(Long.MIN_VALUE);
+        System.out.println(Long.MAX_VALUE);
+        System.out.println();
+    //float    
+        System.out.println(Float.TYPE);
+        System.out.println(Float.SIZE/8+"  byte");
+        System.out.println(Float.MIN_VALUE);
+        System.out.println(Float.MAX_VALUE);
+        System.out.println();
+    //double   
+        System.out.println(Double.TYPE);
+        System.out.println(Double.SIZE/8+" byte");
+        System.out.println(Double.MIN_VALUE);
+        System.out.println(Double.MAX_VALUE);
+        System.out.println();
+    
+	}
+}
