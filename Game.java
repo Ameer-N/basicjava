f0c2593c2e726bf8d33a144248c9f66256b9293d
Ameer <46894200+Ameer-N@users.noreply.github.com>
2020-03-04 12:31:48 +0530

    Update Game.java

--- Game.java
+++ Game.java
@@ -5,66 +5,69 @@ public class Main
 	public static void main(String[] args) {
 	Scanner scanner=new Scanner(System.in); 
 	Random random=new Random();
-	StringBuffer String_Buffer=new StringBuffer();
-	StringBuffer Return_String_Buffer=new StringBuffer();
-	String Selected_Str=null;
-	String [] String_Array={"JOSEPHVIJAY","AJITHKUMAR","VIRATKOHLI","ABDULKALAM","SHAHRUKHKHAN","SIVAKARTHIKEYAN"};
-	int Select_Random_String=random.nextInt(String_Array.length);
-	Selected_Str=String_Array[Select_Random_String];
-    int Selected_Str_Length=Selected_Str.length();
-    for (int i=0;i<Selected_Str_Length;i++){
-        String_Buffer.append("_");
+	StringBuffer string_buffer=new StringBuffer();
+	StringBuffer return_string_buffer=new StringBuffer();
+	String selected_str=null;
+	String [] string_array={"JOSEPHVIJAY","AJITHKUMAR","VIRATKOHLI","ABDULKALAM","SHAHRUKHKHAN","SIVAKARTHIKEYAN"};
+	ArrayList<ArrayList<String> > aaList =new ArrayList<ArrayList<String> >();
+	ArrayList<String> a = new ArrayList<String>();
+	
+	int select_random_string=random.nextInt(string_array.length);
+	selected_str=string_array[select_random_string];
+    int selected_str_length=selected_str.length();
+    for (int i=0;i<selected_str_length;i++){
+        string_buffer.append("_");
     }
  
-    for (int i=0;i<(Math.round(Selected_Str_Length)/3);i++){
-        int Select_Random_String_Buffer=random.nextInt(String_Buffer.length());
+    for (int i=0;i<(Math.round(selected_str_length)/3);i++){
+        int select_random_string_buffer=random.nextInt(string_buffer.length());
        
-        if(String_Buffer.charAt(Select_Random_String_Buffer)=='_'){
-        String_Buffer.setCharAt(Select_Random_String_Buffer,Selected_Str.charAt((Select_Random_String_Buffer)));
+        if(string_buffer.charAt(select_random_string_buffer)=='_'){
+        string_buffer.setCharAt(select_random_string_buffer,selected_str.charAt((select_random_string_buffer)));
         }
        
     }
        System.out.println("Guess Me!!");
           System.out.println("WORD:");
-       for(int i=0;i<String_Buffer.length();i++){
-          System.out.print(String_Buffer.charAt(i)+" ");
+       for(int i=0;i<string_buffer.length();i++){
+          System.out.print(string_buffer.charAt(i)+" ");
        }
        System.out.println();
        int chance=0,increment=1;
-        int chance2=Selected_Str_Length-(Math.round(Selected_Str_Length)/3);
+        int chance2=selected_str_length-(Math.round(selected_str_length)/3);
         do{
         System.out.println("_____________________________________________________________");
         System.out.println("you have "+(chance2--)+" chances to find the word");
          System.out.println();
         System.out.println("Enter the char that you guessed:(Used chance:"+" "+(chance++)+")");    
-        String  User_Str=scanner.next().toUpperCase();
-        char user_char=User_Str.charAt(0);
-        findChar(String_Buffer,Selected_Str,user_char);
-        for(int i=0;i<String_Buffer.length();i++){
-          System.out.print(String_Buffer.charAt(i)+" ");
+        String  user_str=scanner.next().toUpperCase();
+        char user_char=user_str.charAt(0);
+        findChar(string_buffer,selected_str,user_char);
+        for(int i=0;i<string_buffer.length();i++){
+          System.out.print(string_buffer.charAt(i)+" ");
        }
          System.out.println();
         increment++;
   
-        if(Selected_Str.equals(String_Buffer.toString()))
+        if(selected_str.equals(string_buffer.toString()))
         {
              System.out.println("_____________________________________________________________");
             System.out.println("you win!!");
             stop=1;
             break;
         }
-         }while(increment<=Selected_Str_Length-(Math.round(Selected_Str_Length)/3));
+         }while(increment<=selected_str_length-(Math.round(selected_str_length)/3));
          if(stop==0){
              System.out.println("_____________________________________________________________");
              System.out.println("you loss!!");
          }
 	}
-	static String findChar(StringBuffer String_Buffer,String Selected_Str,char user_char){
+	static String findChar(StringBuffer string_buffer,String selected_str,char user_char){
 	    flag=0;
-	   for (int i=0;i<Selected_Str.length();i++){
-	        if(Selected_Str.charAt(i)==user_char && String_Buffer.charAt(i)!=user_char){
+	   for (int i=0;i<selected_str.length();i++){
+	        if(selected_str.charAt(i)==user_char && string_buffer.charAt(i)!=user_char){
 	              flag=1;
-	              String_Buffer.setCharAt(i,user_char);
+	              string_buffer.setCharAt(i,user_char);
 	        }
 	        
 	    }
@@ -76,7 +79,7 @@ public class Main
 	        System.out.println("wrong guess,try again!!");
 	        
 	    }
-	    return String_Buffer.toString();
+	    return string_buffer.toString();
 	}
 	
 }
